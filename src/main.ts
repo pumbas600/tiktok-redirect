4b2a2fad67e3ae67ee44c768c91df447f92969fb
Josh Jeffers <josh@letterboxd.com>
2025-05-18 10:22:56 +1200

    feat: setup typescript and prettier

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+const IS_FIREFOX = typeof browser !== "undefined";
+const IS_CHROME = typeof chrome !== "undefined";
+
+function setup() {
+  // browser.webRequest.onBeforeRequest.addListener();
+}
+
+setup();
